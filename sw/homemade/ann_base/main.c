ca0d5b587a7f1c670c846362d95864780e32040e
abe-pratomo <abededetab32@gmail.com>
2026-07-20 22:19:29 +0700

    Made ANN programs consistent across versions

--- sw/homemade/ann_base/main.c
+++ sw/homemade/ann_base/main.c
@@ -177,10 +177,10 @@ int main() {
 
     // Print taste values
     neorv32_uart0_printf("Input Taste Values:\n");
-    neorv32_uart0_printf("Sourness  : "); print_q16(inputs[0]); neorv32_uart0_printf("\n");
-    neorv32_uart0_printf("Sweetness : "); print_q16(inputs[1]); neorv32_uart0_printf("\n");
-    neorv32_uart0_printf("Saltiness : "); print_q16(inputs[2]); neorv32_uart0_printf("\n");
-    neorv32_uart0_printf("Spiciness : "); print_q16(inputs[3]); neorv32_uart0_printf("\n");
+    neorv32_uart0_printf("Sourness : "); print_q16(inputs[0]); neorv32_uart0_printf("\n");
+    neorv32_uart0_printf("Sweetness: "); print_q16(inputs[1]); neorv32_uart0_printf("\n");
+    neorv32_uart0_printf("Saltiness: "); print_q16(inputs[2]); neorv32_uart0_printf("\n");
+    neorv32_uart0_printf("Spiciness: "); print_q16(inputs[3]); neorv32_uart0_printf("\n");
 
     // Start calculation
     uint32_t start_time = neorv32_cpu_csr_read(CSR_CYCLE);
@@ -218,11 +218,11 @@ int main() {
     uint32_t elapsed_time_ns    = (uint32_t)(((uint64_t)elapsed_cycles * 1000ULL) / (NEORV32_CLK / 1000000));
 
     // Print results
-    neorv32_uart0_printf("\nOutput Values :\n");
+    neorv32_uart0_printf("\nOutput Values:\n");
     for (int i = 0; i < o_neuron; i++) {
-        neorv32_uart0_printf("%s :", i ? "Untastiness" : "Tastiness  "); print_q16(outputs[i]); neorv32_uart0_printf("\n");
+        neorv32_uart0_printf("%s:", i ? "Untastiness" : "Tastiness  "); print_q16(outputs[i]); neorv32_uart0_printf("\n");
     }
-    neorv32_uart0_printf("\nConclusion : %s\n", (outputs[0] > outputs[1]) ? "Tasty" : "Not Tasty");
+    neorv32_uart0_printf("\nConclusion: %s\n", (outputs[0] > outputs[1]) ? "Tasty" : "Not Tasty");
     neorv32_uart0_printf("\nElapsed Time: %u cycles (%u ns)\n", elapsed_cycles, elapsed_time_ns);
 
     // Finish execution
